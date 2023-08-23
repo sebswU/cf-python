df09e811886ba0ae4b941991ba45fd61653ec31e
Sebastian Wu <68798342+sebswU@users.noreply.github.com>
2023-08-22 23:32:19 -0400

    add model

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import nextcord
-import numpy as np
 import requests
 from nextcord.ext import commands
 import re
 import string
 import nltk
+import tensorflow as tf
+import tensorflow_hub as hub
 from dotenv import load_dotenv
+import os
 load_dotenv()
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -14,13 +16,29 @@ user = re.compile(r"(?i)@[a-z0-9_]+")
 regex_list = [web_address, user]
 
 bot = commands.Bot(command_prefix='$')
-def clean(input):
+
+embed = hub.KerasLayer("https://tfhub.dev/google/nnlm-en-dim50/2",input_shape=[],dtype=tf.string,trainable=True)
+model = tf.keras.Sequential([
+    embed,
+    tf.keras.layers.Dense(32,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(32,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+weight_dir = os.path.dirname("model/")
+weights = tf.train.latest_checkpoint(weight_dir)
+
+model.load_weights(weights)
+
+def clean(inp):
     """clean from stopwords and other stuff, convert to list, back to string"""
-    if isinstance(input, float):
-        input = 'a'
+    if isinstance(inp, float):
+        inp = 'a'
     STOPWORDS = stopwords.words('english') 
     # Check characters to see if they are in punctuation
-    nopunc = [char for char in input if char not in string.punctuation and all(regex.search(char.lower()) is None for regex in regex_list)]
+    nopunc = [char for char in inp.split("\n") if char not in string.punctuation and all(re.search(regex, char.lower()) is None for regex in regex_list)]
   
     # Join the characters again to form the string.
     nopunc = ''.join(nopunc)
@@ -30,16 +48,14 @@ def clean(input):
   
 @bot.event
 async def on_message(message):
-    if message.author == bot:
-        return
+    if message.author.bot == True:
+        pass
     else:
-        url = 'http://localhost:8501/1/models/model:predict'
         tester = [clean(message.content)]
-        data = {"instances":tester}
-        response = requests.get(url, data=data,headers={})["predictions"]
+        response = model.predict(tester)
         print('test: your score is ', response)
         #tester is the thing put into the dataset
-        await message.channel.send('test: your score is ', response)
+        await message.channel.send(f'test: your score is {response}')
 
 class MyClient(nextcord.Client):
     async def on_message(self, message):
@@ -48,4 +64,16 @@ class MyClient(nextcord.Client):
 
         if message.content.startswith('$hello'):
             await message.channel.send('Hello World!')   
-bot.run("TOKEN")
+
+@bot.command
+def false_pos():
+    pass
+
+@bot.command
+def false_neg():
+    pass
+
+
+
+
+bot.run("token")
